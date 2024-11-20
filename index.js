02006cdcb9aebf9d9afb2d4c58af497ec47da672
Volodymyr025 <Fudo2790@gmail.com>
2024-11-20 12:29:11 +0200

    link

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const io = require("socket.io")(8080, {
-  cors: { origin: "https://socket-io-ngr4.onrender.com" },
+  cors: { origin: "https://chat-online-iota.vercel.app/" },
 });
 
 const users = [];
